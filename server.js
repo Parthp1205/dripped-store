69d7c1c4b23ef8700b9b613f00f67d5192a9083f
Parth Patel <162091862+Parthp1205@users.noreply.github.com>
2025-07-09 20:45:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use('/images', express.static(path.join(__dirname, 'public/images')));
 
 
-
 const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET,
