894ce6a9dfd6d2e23e6e9be02e41c1c6cf811ad7
Parth <parthp1205@gmail.com>
2025-07-09 16:38:27 +0530

    Add images and fix for render deployment

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
+
 const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET,
